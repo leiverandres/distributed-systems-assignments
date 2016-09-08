c8cf0bbef50a7912bb9057cedd7f6c4b26a88245
Leiver Andres Campeon <Leiver Andres Campeon>
2016-09-08 15:41:00 -0500

    done super simple example with socket

--- Berkeley/leiverandres/clientBerkeley.py
+++ Berkeley/leiverandres/clientBerkeley.py
@@ -1,23 +1,32 @@
 import socket
 import sys
-import datetime
+import time
 
 if __name__ == '__main__':
-	host, port = 'localhost', 5050
+	host, port = 'localhost', 5000
 
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	print "Connecting to {0}:{1}".format(host, port)
 	try:
 		sock.connect((host, port))
 	except socket.error as e:
-	    print(str(e))
-
+		print(str(e))
 	try:
-		time = datetime.datetime.now().time()
-		sock.sendall(str(time))
-		print "Sending data to server"
-		data  = sock.recv(1000)
-		print "Getting data from server: {0}".format(data)
+		while True:
+			data  = sock.recv(1000).split(' ')
+			print "Getting time from server: {0}".format(data)
+			if data[0] == 'get':
+				offset = time.time() - float(data[1])
+				print "local time: {0}".format(offset)
+				sock.sendall(str(offset))
+				print "Sending time to server"
+			elif data[0] == 'post':
+				local_time = float(data[1])
+				print "new time: {0}".format(local_time)
+			elif data[0] == '':
+				break
+	except socket.error as e:
+		print(str(e))
 	finally:
 		print "closing connection"
 		sock.close()
